c3ad781e571a6a6eabf1783ef5fbf4089c6dcc0e
Eugene Ware <eugene@noblesamurai.com>
2013-09-09 13:38:08 +1000

    use level-orm

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var level = require('level'),
     bytewise = require('bytewise/hex'),
     timestamp = require('monotonic-timestamp'),
     sublevel = require('level-sublevel'),
+    Models = require('level-orm'),
     after = require('after');
 
 module.exports = LevelMicroBlog;
@@ -27,66 +28,11 @@ LevelMicroBlog.prototype.close = function(cb) {
 }
 
 /**
- * Model
- */
-
-function Models(mblog, name, key) {
-  this.name = name;
-  this.key = key;
-  this.mblog = mblog;
-  this[this.name] = this.mblog.db.sublevel(name);
-}
-
-Models.prototype.all = function(cb) {
-  var models = [];
-  this[this.name].createReadStream().pipe(through(write, end));
-  function write(model) {
-    models.push(model.value);
-  }
-  function end() {
-    cb(null, models);
-  }
-};
-
-Models.prototype.save = function(model, cb) {
-  var key = this.getKey(model);
-  this[this.name].put(key, model, function (err) {
-    if (err) return cb(err);
-    cb(null, key);
-  });
-};
-
-Models.prototype.get = function(key, cb) {
-  this[this.name].get(key, function (err, data) {
-    if (err) return cb(err);
-    cb(null, data);
-  });
-};
-
-Models.prototype.del = function(key, cb) {
-  this[this.name].del(key, cb);
-};
-
-Models.prototype.getKey = function(model) {
-  if (typeof model[this.key] === 'undefined' && this.keyfn) {
-    var key = this.keyfn(model);
-    model[this.key] = key;
-    return key;
-  } else if (Array.isArray(this.key)) {
-    return this.key.map(function (prop) {
-      return model[prop];
-    });
-  } else {
-    return model[this.key];
-  }
-};
-
-/**
  * User
  */
 
-function Users(mblog) {
-  Models.call(this, mblog, 'users', 'handle');
+function Users(container) {
+  Models.call(this, container, 'users', 'handle');
 }
 util.inherits(Users, Models);
 
@@ -95,12 +41,12 @@ Users.prototype.message = function(handle, msg, cb) {
   this.get(handle, function (err, user) {
     if (err) return cb(err);
     var message = { handle: user.handle, message: msg };
-    self.mblog.Messages.save(message, function (err, id) {
+    self.container.Messages.save(message, function (err, id) {
       if (err) return cb(err);
       var next = after(user.followers.length, done);
       user.followers.forEach(function (follower) {
         message.to = follower;
-        self.mblog.Feed.save(message, next);
+        self.container.Feed.save(message, next);
       });
       function done(err) {
         if (err) return cb(err);
@@ -114,8 +60,8 @@ Users.prototype.message = function(handle, msg, cb) {
  * Message
  */
 
-function Messages(mblog) {
-  Models.call(this, mblog, 'messages', 'id');
+function Messages(container) {
+  Models.call(this, container, 'messages', 'id');
 }
 util.inherits(Messages, Models);
 
@@ -124,8 +70,8 @@ Messages.prototype.keyfn = timestamp;
 /**
  * Feed
  */
-function Feed(mblog) {
-  Models.call(this, mblog, 'feed', ['to', 'id']);
+function Feed(container) {
+  Models.call(this, container, 'feed', ['to', 'id']);
 }
 util.inherits(Feed, Models);
 
